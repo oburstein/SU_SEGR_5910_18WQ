95fac9d49e032c45f6fe360bc30d5c890807a443
Michaeljon Miller <michaeljon.miller@outlook.com>
2018-01-31 21:13:12 -0800

    initial checkin to get the right paths

---
+++ containers/ipshow.js
@@ -0,0 +1,30 @@
+const fs = require('fs'),
+  http = require('http'),
+  os = require('os');
+
+var interfaces = os.networkInterfaces(),
+  ipdata,
+  addresses = [];
+
+fs.readFile('./hostip', 'utf8', function (err, data) {
+  console.log(data);
+  console.log(interfaces);
+  ipdata = data;
+});
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.write("host ip: " + ipdata + " \nother ip addresses: \n");
+
+  for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+      let address = interfaces[k][k2];
+
+      if (address.family === "IPv4") {
+        response.write(address.address + "\n");
+      }
+    }
+  }
+
+  response.end();
+}).listen(8080);
