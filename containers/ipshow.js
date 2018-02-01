8b311330cd5a3956f4e8fefeec501c652a33e94d
Michaeljon Miller <michaeljon.miller@outlook.com>
2018-01-31 21:29:59 -0800

    dealing with older nodejs

--- containers/ipshow.js
+++ containers/ipshow.js
@@ -18,7 +18,7 @@ var server = http.createServer(function (request, response) {
 
   for (var k in interfaces) {
     for (var k2 in interfaces[k]) {
-      let address = interfaces[k][k2];
+      var address = interfaces[k][k2];
 
       if (address.family === "IPv4") {
         response.write(address.address + "\n");
